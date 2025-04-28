9b53ae21e802121cc786da1a724cfc2b4da922f8
Dongwoo Shin <socra167@gmail.com>
2025-04-28 21:08:05 +0900

    :wrench: chore: STT 동작 테스트를 위한 임시 CommandLineRunner

---
+++ src/main/java/com/mallang/mallang_backend/global/util/TestRunner.java
@@ -0,0 +1,25 @@
+package com.mallang.mallang_backend.global.util;
+
+import java.io.File;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class TestRunner implements CommandLineRunner {
+
+	private final ClovaSpeechClient clovaSpeechClient;
+
+	@Override
+	public void run(String... args) throws Exception {
+		NestRequestEntity requestEntity = new NestRequestEntity();
+		final String result =
+			clovaSpeechClient.upload(new File("./uploads/audio_8cf7c349-a51d-4f75-9428-4259bfbdb8441745799537457.mp3"), requestEntity);
+		//final String result = clovaSpeechClient.url("file URL", requestEntity);
+		//final String result = clovaSpeechClient.objectStorage("Object Storage key", requestEntity);
+		System.out.println(result);
+	}
+}
