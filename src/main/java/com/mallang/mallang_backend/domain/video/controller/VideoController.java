fd4b6ede78835ccaea909cad2d01e9b9554c3259
Dongwoo Shin <socra167@gmail.com>
2025-04-28 10:09:24 +0900

    :sparkles: feat: `VideoController`에 `VideoSerice` 연결

--- src/main/java/com/mallang/mallang_backend/domain/video/controller/VideoController.java
+++ src/main/java/com/mallang/mallang_backend/domain/video/controller/VideoController.java
@@ -1,11 +1,18 @@
 package com.mallang.mallang_backend.domain.video.controller;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.mallang.mallang_backend.domain.video.service.VideoService;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/v1/video")
 @RequiredArgsConstructor
 public class VideoController {
+
+	@Autowired
+	private final VideoService videoService;
 }
--- src/main/java/com/mallang/mallang_backend/domain/video/service/impl/VideoServiceImpl.java
+++ src/main/java/com/mallang/mallang_backend/domain/video/service/impl/VideoServiceImpl.java
@@ -1,11 +1,14 @@
 package com.mallang.mallang_backend.domain.video.service.impl;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.mallang.mallang_backend.domain.video.service.VideoService;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
-public class VideoServiceImpl {
+public class VideoServiceImpl implements VideoService {
 }
