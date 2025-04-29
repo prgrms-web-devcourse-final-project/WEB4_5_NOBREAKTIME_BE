eff7f9f1fdc8078d8073ffc744e9a38e0de08fa3
wkdan <jmy8876@gmail.com>
2025-04-29 18:42:06 +0900

    ♻️refactor: RetryLoggingAspect Lombok @Slf4j로 변경

--- src/main/java/com/mallang/mallang_backend/global/config/RetryLoggingAspect.java
+++ src/main/java/com/mallang/mallang_backend/global/config/RetryLoggingAspect.java
@@ -6,14 +6,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @Retryable로 주석된 메서드에서 예외 발생할 때 마다 로그 남기기 위한 클래스
  */
+@Slf4j
 @Aspect
 @Component
 public class RetryLoggingAspect {
-
-	private static final Logger log = LoggerFactory.getLogger(RetryLoggingAspect.class);
 
 	// Retryable이 붙은 메서드가 IOException을 던질 때마다 로그를 찍음
 	@AfterThrowing(pointcut = "@annotation(org.springframework.retry.annotation.Retryable)", throwing = "ex")
