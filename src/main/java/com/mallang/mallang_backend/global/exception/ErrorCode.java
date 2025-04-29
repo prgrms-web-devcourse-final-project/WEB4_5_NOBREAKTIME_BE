1e10b7fdf3ca0b76ed32f065431b765700ab32e3
gwony <moonsky1124@naver.com>
2025-04-29 10:57:17 +0900

    ✨ feat : GPT, 단어 저장 관련 에러 코드 추가

--- src/main/java/com/mallang/mallang_backend/global/exception/ErrorCode.java
+++ src/main/java/com/mallang/mallang_backend/global/exception/ErrorCode.java
@@ -12,7 +12,17 @@ public enum ErrorCode {
     USER_NOT_FOUND("404-1", "user.not.found", HttpStatus.NOT_FOUND),
 
     // Token Errors
-    TOKEN_EXPIRED("401-1", "token.expired", HttpStatus.UNAUTHORIZED);
+    TOKEN_EXPIRED("401-1", "token.expired", HttpStatus.UNAUTHORIZED),
+
+    // GPT Errors
+    GPT_RESPONSE_PARSE_FAIL("500-1", "gpt.response.parse.fail", HttpStatus.INTERNAL_SERVER_ERROR),
+    GPT_API_CALL_FAILED("500-4", "gpt.api.call.failed", HttpStatus.INTERNAL_SERVER_ERROR),
+    GPT_RESPONSE_EMPTY("500-5", "gpt.response.empty", HttpStatus.INTERNAL_SERVER_ERROR),
+
+
+    // Word Errors (추가)
+    WORD_SAVE_FAILED("500-2", "word.save.failed", HttpStatus.INTERNAL_SERVER_ERROR),
+    WORD_PARSE_FAILED("500-3", "word.parse.failed", HttpStatus.INTERNAL_SERVER_ERROR);
 
 
     private final String code;
