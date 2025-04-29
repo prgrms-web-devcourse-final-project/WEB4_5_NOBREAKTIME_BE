7516cbcffa6e6b006aeb7dcc301135b17f69af10
sameom1048 <samvision99@naver.com>
2025-04-29 09:58:37 +0900

    :recycle: refactor: `ClovaSpeechClientImpl` 불필요한 메서드 삭제

--- src/main/java/com/mallang/mallang_backend/global/util/ClovaSpeechClientImpl.java
+++ src/main/java/com/mallang/mallang_backend/global/util/ClovaSpeechClientImpl.java
@@ -2,15 +2,12 @@ package com.mallang.mallang_backend.global.util;
 
 import java.io.File;
 import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ContentType;
-import org.apache.http.entity.StringEntity;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
@@ -50,58 +47,6 @@ public class ClovaSpeechClientImpl implements ClovaSpeechClient {
 			new BasicHeader("Accept", "application/json"),
 			new BasicHeader("X-CLOVASPEECH-API-KEY", secret),
 		};
-	}
-
-	/**
-	 * recognize media using URL (외부 파일 URL로 음성 인식 요청)
-	 * @param url required, the media URL (필수 파라미터, 외부 파일 URL)
-	 * @param nestRequestEntity optional (필수 파라미터가 아님)
-	 * @return string (문자열 반환)
-	 */
-	public String url(String url, NestRequestEntity nestRequestEntity) {
-		HttpPost httpPost = new HttpPost(invoke_url + "/recognizer/url");
-		httpPost.setHeaders(createHeaders());
-		Map<String, Object> body = new HashMap<>();
-		body.put("url", url);
-		body.put("language", nestRequestEntity.getLanguage());
-		body.put("completion", nestRequestEntity.getCompletion());
-		body.put("callback", nestRequestEntity.getCallback());
-		body.put("userdata", nestRequestEntity.getCallback());
-		body.put("wordAlignment", nestRequestEntity.getWordAlignment());
-		body.put("fullText", nestRequestEntity.getFullText());
-		body.put("forbiddens", nestRequestEntity.getForbiddens());
-		body.put("boostings", nestRequestEntity.getBoostings());
-		body.put("diarization", nestRequestEntity.getDiarization());
-		body.put("sed", nestRequestEntity.getSed());
-		HttpEntity httpEntity = new StringEntity(gson.toJson(body), ContentType.APPLICATION_JSON);
-		httpPost.setEntity(httpEntity);
-		return execute(httpPost);
-	}
-
-	/**
-	 * recognize media using Object Storage (네이버 클라우드 플랫폼의 Object Storage 내 파일 URL로 음성 인식 요청)
-	 * @param dataKey required, the Object Storage key (필수 파라미터, Object Storage 키 값)
-	 * @param nestRequestEntity optional (필수 파라미터가 아님)
-	 * @return string (문자열 반환)
-	 */
-	public String objectStorage(String dataKey, NestRequestEntity nestRequestEntity) {
-		HttpPost httpPost = new HttpPost(invoke_url + "/recognizer/object-storage");
-		httpPost.setHeaders(createHeaders());
-		Map<String, Object> body = new HashMap<>();
-		body.put("dataKey", dataKey);
-		body.put("language", nestRequestEntity.getLanguage());
-		body.put("completion", nestRequestEntity.getCompletion());
-		body.put("callback", nestRequestEntity.getCallback());
-		body.put("userdata", nestRequestEntity.getCallback());
-		body.put("wordAlignment", nestRequestEntity.getWordAlignment());
-		body.put("fullText", nestRequestEntity.getFullText());
-		body.put("forbiddens", nestRequestEntity.getForbiddens());
-		body.put("boostings", nestRequestEntity.getBoostings());
-		body.put("diarization", nestRequestEntity.getDiarization());
-		body.put("sed", nestRequestEntity.getSed());
-		StringEntity httpEntity = new StringEntity(gson.toJson(body), ContentType.APPLICATION_JSON);
-		httpPost.setEntity(httpEntity);
-		return execute(httpPost);
 	}
 
 	/**
