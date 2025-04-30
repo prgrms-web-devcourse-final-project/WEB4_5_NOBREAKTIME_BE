199359d361c463e5b9460d9ce5f2ceab14478543
Dongwoo Shin <socra167@gmail.com>
2025-04-30 10:31:54 +0900

    :sparkles: feat: 인증 없이 ID가 1인 회원으로 단어장 추가되도록 변경

--- src/main/java/com/mallang/mallang_backend/domain/voca/wordbook/controller/WordbookController.java
+++ src/main/java/com/mallang/mallang_backend/domain/voca/wordbook/controller/WordbookController.java
@@ -1,7 +1,6 @@
 package com.mallang.mallang_backend.domain.voca.wordbook.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mallang.mallang_backend.domain.member.entity.Member;
+import com.mallang.mallang_backend.domain.member.repository.MemberRepository;
 import com.mallang.mallang_backend.domain.voca.wordbook.dto.AddWordToWordbookListRequest;
 import com.mallang.mallang_backend.domain.voca.wordbook.service.WordbookService;
 import com.mallang.mallang_backend.global.dto.RsData;
@@ -21,20 +21,22 @@ import lombok.RequiredArgsConstructor;
 public class WordbookController {
 
 	private final WordbookService wordbookService;
+	private final MemberRepository memberRepository;
 
 	/**
 	 * 영상 학습 중 1개 이상의 단어를 추가합니다.
 	 * @param wordbookId 단어들을 추가할 단어장 ID
 	 * @param request 영상 ID, 추가할 단어, 단어의 원래 문장 Request 객체
-	 * @param member 로그인한 회원
 	 * @return 단어 추가 성공 응답
 	 */
 	@PostMapping("/{wordbookId}/words")
 	public ResponseEntity<RsData<Void>> addWords(
 		@PathVariable Long wordbookId,
-		@RequestBody AddWordToWordbookListRequest request,
-		@AuthenticationPrincipal Member member
+		@RequestBody AddWordToWordbookListRequest request
 	) {
+		// 추후 인증 필터 추가되면 로그인한 회원으로 변경
+		Member member = memberRepository.findById(1L).get();
+
 		wordbookService.addWords(wordbookId, request, member);
 		return ResponseEntity.ok(new RsData<>(
 			"200-1",
