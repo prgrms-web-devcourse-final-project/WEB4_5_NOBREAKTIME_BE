91275cafa9c42c572c35062465b0597de39f5021
sameom1048 <samvision99@naver.com>
2025-05-01 22:20:28 +0900

    :wrench: chore: develop branch 충돌 해결

---
+++ src/main/java/com/mallang/mallang_backend/domain/voca/wordbook/repository/WordbookRepository.java
@@ -0,0 +1,15 @@
+package com.mallang.mallang_backend.domain.voca.wordbook.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mallang.mallang_backend.domain.member.entity.Member;
+import com.mallang.mallang_backend.domain.voca.wordbook.entity.Wordbook;
+
+public interface WordbookRepository extends JpaRepository<Wordbook, Long> {
+	Optional<Wordbook> findByIdAndMember(Long wordbookId, Member member);
+
+	List<Wordbook> findByMember(Member member);
+}
