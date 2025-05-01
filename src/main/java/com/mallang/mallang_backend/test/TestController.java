57dd8bc1c51a02954df9e7accdcd1bd8b7757095
sun <suunn001@gmail.com>
2025-05-01 19:20:32 +0900

    :sparkles: Feat: TestController 추가 (로그인 테스트 용도)

---
+++ src/main/java/com/mallang/mallang_backend/test/TestController.java
@@ -0,0 +1,32 @@
+package com.mallang.mallang_backend.test;
+
+import com.mallang.mallang_backend.domain.member.entity.Member;
+import com.mallang.mallang_backend.domain.member.repository.MemberRepository;
+import com.mallang.mallang_backend.global.dto.RsData;
+import com.mallang.mallang_backend.global.filter.CustomUserDetails;
+import com.mallang.mallang_backend.global.filter.Login;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * 로그인 테스트 컨트롤러입니다.
+ */
+@RestController
+@RequiredArgsConstructor
+public class TestController {
+
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/api/test")
+    public ResponseEntity<?> test(@Login CustomUserDetails userDetails) {
+        Long memberId = userDetails.getMemberId();
+        Member findMember = memberRepository.findById(memberId).get();
+
+        return ResponseEntity.status(200).body("로그인 성공~ " +
+                "사용자 아이디 값: " + findMember.getId());
+
+    }
+}
