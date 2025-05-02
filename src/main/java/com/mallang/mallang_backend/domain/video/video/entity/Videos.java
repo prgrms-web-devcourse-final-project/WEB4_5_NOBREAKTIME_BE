c0c872e6a2d8bce80077ad88a21ed73331fc74b6
gwony <moonsky1124@naver.com>
2025-05-02 22:41:13 +0900

    ♻️ refactor: ERD 변경으로 인해 엔티티 수정

--- src/main/java/com/mallang/mallang_backend/domain/video/video/entity/Videos.java
+++ src/main/java/com/mallang/mallang_backend/domain/video/video/entity/Videos.java
@@ -1,11 +1,15 @@
 package com.mallang.mallang_backend.domain.video.video.entity;
 
+import com.mallang.mallang_backend.domain.video.subtitle.entity.Subtitle;
 import com.mallang.mallang_backend.global.common.Language;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Entity
@@ -28,6 +32,9 @@ public class Videos {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Language language;
+
+    @OneToMany(mappedBy = "videos", fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
+    private List<Subtitle> subtitles = new ArrayList<>();
 
     @Builder
     private Videos(
