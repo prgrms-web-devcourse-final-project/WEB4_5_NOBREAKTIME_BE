9ee452b75e5c62a490424204c792c4a4d3164081
sun <suunn001@gmail.com>
2025-05-03 16:00:01 +0900

    :fire: Merge develop branch into main

--- src/main/java/com/mallang/mallang_backend/domain/sentence/expressions/repository/ExpressionsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mallang.mallang_backend.domain.sentence.expressions.repository;
-
-import com.mallang.mallang_backend.domain.sentence.expressions.entity.Expressions;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExpressionsRepository extends JpaRepository<Expressions, Long> {
-}
--- src/main/java/com/mallang/mallang_backend/domain/video/repository/VideoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mallang.mallang_backend.domain.video.repository;
-
-import com.mallang.mallang_backend.domain.video.entity.Video;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VideoRepository extends JpaRepository<Video, Long> {
-}
