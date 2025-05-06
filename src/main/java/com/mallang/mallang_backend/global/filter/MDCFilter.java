cc03cc19630344a1cdf922bb4d953909296e9fbf
sun <suunn001@gmail.com>
2025-05-06 18:39:36 +0900

    ✨ feat: 트랜잭션 ID 를 꺼낼 수 있는 MDC 서블릿 필터 생성

---
+++ src/main/java/com/mallang/mallang_backend/global/filter/MDCFilter.java
@@ -0,0 +1,28 @@
+package com.mallang.mallang_backend.global.filter;
+
+import jakarta.servlet.*;
+import org.slf4j.MDC;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@Component
+public class MDCFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        String transactionId = UUID.randomUUID().toString();
+        MDC.put("transactionId", transactionId); // 또는 "traceId"
+        try {
+            chain.doFilter(request, response);
+        } finally {
+            MDC.remove("transactionId");
+        }
+    }
+}
