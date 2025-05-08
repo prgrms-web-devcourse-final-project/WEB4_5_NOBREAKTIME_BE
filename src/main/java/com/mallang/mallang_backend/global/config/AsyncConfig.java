6150ace264222dbdfbd9649eb18c59b93a46f099
sun <suunn001@gmail.com>
2025-05-08 12:39:57 +0900

    :sparkles: feat: 비동기 처리 스레드 풀 추가

--- src/main/java/com/mallang/mallang_backend/global/config/AsyncConfig.java
+++ src/main/java/com/mallang/mallang_backend/global/config/AsyncConfig.java
@@ -1,11 +1,13 @@
 package com.mallang.mallang_backend.global.config;
 
 import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.security.task.DelegatingSecurityContextAsyncTaskExecutor;
 
 @Configuration
 @EnableAsync
@@ -45,5 +47,26 @@ public class AsyncConfig {
 		executor.setThreadNamePrefix("video-");
 		executor.initialize();
 		return executor;
+	}
+
+	@Bean(name = "securityTaskExecutor")
+	public Executor securityTaskExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(5); // 기본 스레드 풀 사이즈
+		executor.setMaxPoolSize(10); // 최대 스레드 풀 사이즈
+		executor.setQueueCapacity(20); // 작업 대기 큐 크기
+		executor.setKeepAliveSeconds(60); // 60 초 대기 후 초과 스레드 삭제
+		executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy()); // 거절 정책
+		executor.setThreadNamePrefix("Social-Async-");
+		executor.initialize();
+
+		/**
+		 * 시큐리티 -> 스레드 로컬에 사용자 인증 정보를 저장
+		 * 스레드 풀 사용 시, 새 스레드는 기존 스레드의 ThreadLocal 값을 상속받지 않아 SecurityContext가 유실
+		 *
+		 * DelegatingSecurityContextAsyncTaskExecutor :
+		 * 작업 실행 시 SecurityContext 를 새 스레드에 복사하는 래퍼를 생성
+		 */
+		return new DelegatingSecurityContextAsyncTaskExecutor(executor);
 	}
 }
