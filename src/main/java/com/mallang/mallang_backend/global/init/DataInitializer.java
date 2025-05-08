c98b0febd08d1c0ed8cac3de108c2bcff8207257
Dongwoo Shin <socra167@gmail.com>
2025-05-08 12:06:11 +0900

    :config: chore: DataInitalizer 초기화 멤버 언어를 ENGLISH, STANDARD로 수정, 초기 단어장과 표현함 생성

--- src/main/java/com/mallang/mallang_backend/global/init/DataInitializer.java
+++ src/main/java/com/mallang/mallang_backend/global/init/DataInitializer.java
@@ -1,21 +1,28 @@
 package com.mallang.mallang_backend.global.init;
 
-import com.mallang.mallang_backend.domain.member.entity.LoginPlatform;
-import com.mallang.mallang_backend.domain.member.entity.Member;
-import com.mallang.mallang_backend.domain.member.repository.MemberRepository;
-import com.mallang.mallang_backend.global.common.Language;
-import com.mallang.mallang_backend.global.token.TokenPair;
-import com.mallang.mallang_backend.global.token.TokenService;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import java.io.IOException;
+import java.util.List;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
-import java.io.*;
-import java.util.List;
+import com.mallang.mallang_backend.domain.member.entity.LoginPlatform;
+import com.mallang.mallang_backend.domain.member.entity.Member;
+import com.mallang.mallang_backend.domain.member.entity.Subscription;
+import com.mallang.mallang_backend.domain.member.repository.MemberRepository;
+import com.mallang.mallang_backend.domain.sentence.expressionbook.entity.ExpressionBook;
+import com.mallang.mallang_backend.domain.sentence.expressionbook.repository.ExpressionBookRepository;
+import com.mallang.mallang_backend.domain.voca.wordbook.entity.Wordbook;
+import com.mallang.mallang_backend.domain.voca.wordbook.repository.WordbookRepository;
+import com.mallang.mallang_backend.global.common.Language;
+import com.mallang.mallang_backend.global.token.TokenPair;
+import com.mallang.mallang_backend.global.token.TokenService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Component
@@ -25,9 +32,13 @@ public class DataInitializer implements CommandLineRunner {
     private final MemberRepository memberRepository;
     private final TokenService tokenService;
 
+    private final WordbookRepository wordbookRepository;
+    private final ExpressionBookRepository expressionBookRepository;
+
     @Override
     public void run(String... args) throws Exception {
         Member basicUser = createTestUser();
+        // Member basicUser = memberRepository.findById(1L).get();
         TokenPair tokenPair1 = tokenService.createTokenPair(
                 basicUser.getId(),
                 basicUser.getSubscription().getRoleName()
@@ -48,10 +59,17 @@ public class DataInitializer implements CommandLineRunner {
                 .email("google123@gmail.com")
                 .nickname("TestUser1")
                 .loginPlatform(LoginPlatform.GOOGLE)
-                .language(Language.NONE)
+                .language(Language.ENGLISH)
                 .profileImageUrl("https://team07-mallang-bucket.s3.ap-northeast-2.amazonaws.com/profile.jpg")
                 .build();
+        testUser.updateSubscription(Subscription.STANDARD);
+        testUser = memberRepository.save(testUser);
 
+        List<Wordbook> wordbooks = Wordbook.createDefault(testUser);
+        wordbookRepository.saveAll(wordbooks);
+
+        List<ExpressionBook> expressionBooks = ExpressionBook.createDefault(testUser);
+        expressionBookRepository.saveAll(expressionBooks);
         /*
         스탠다드 유저가 필요할 때 -> 하단의 리턴을 주석처리 후 사용
         memberRepository.save(testUser);
@@ -64,7 +82,7 @@ public class DataInitializer implements CommandLineRunner {
         testUser.updateSubscription(Subscription.PREMIUM);
         return testUser;*/
 
-        return memberRepository.save(testUser);
+        return testUser;
     }
 
     // 시큐리티 객체에 인증 정보 추가
