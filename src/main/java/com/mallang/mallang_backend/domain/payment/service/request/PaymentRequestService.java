fd82a257e93e46cb8ff1a613e49679680dd53f81
sun <suunn001@gmail.com>
2025-05-15 03:57:59 +0900

    ✨ feat: 결제 후 부가적인 이벤트를 발행하는 리스너 구현 및 이에 따른 자잘한 변경

--- src/main/java/com/mallang/mallang_backend/domain/payment/service/request/PaymentRequestService.java
+++ src/main/java/com/mallang/mallang_backend/domain/payment/service/request/PaymentRequestService.java
@@ -1,11 +1,10 @@
 package com.mallang.mallang_backend.domain.payment.service.request;
 
-import com.mallang.mallang_backend.domain.payment.dto.PaymentRequest;
-import com.mallang.mallang_backend.domain.payment.dto.PaymentSimpleRequest;
+import com.mallang.mallang_backend.domain.payment.dto.request.PaymentRequest;
+import com.mallang.mallang_backend.domain.payment.dto.request.PaymentSimpleRequest;
 
 public interface PaymentRequestService {
 
-    PaymentRequest createPaymentRequest(String idempotencyKey,
-                                               Long memberId,
-                                               PaymentSimpleRequest simpleRequest);
+    PaymentRequest createPaymentRequest(Long memberId,
+                                        PaymentSimpleRequest simpleRequest);
 }
