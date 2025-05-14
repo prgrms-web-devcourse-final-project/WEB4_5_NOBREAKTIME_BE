c22093ccf67b800cea1553d5d1bf0cb5936d28b8
sun <suunn001@gmail.com>
2025-05-15 03:56:41 +0900

    ✨ feat: 결제 요청 성공 -> 승인 후 결과를 반환하는 API / 결제 요청 실패 API 구현

--- src/main/java/com/mallang/mallang_backend/domain/payment/controller/PaymentController.java
+++ src/main/java/com/mallang/mallang_backend/domain/payment/controller/PaymentController.java
@@ -1,24 +1,40 @@
 package com.mallang.mallang_backend.domain.payment.controller;
 
 import com.mallang.mallang_backend.domain.payment.docs.PaymentRequestDocs;
-import com.mallang.mallang_backend.domain.payment.dto.PaymentRequest;
-import com.mallang.mallang_backend.domain.payment.dto.PaymentSimpleRequest;
+import com.mallang.mallang_backend.domain.payment.dto.after.PaymentFailureRequest;
+import com.mallang.mallang_backend.domain.payment.dto.approve.PaymentResponse;
+import com.mallang.mallang_backend.domain.payment.dto.approve.PaymentApproveRequest;
+import com.mallang.mallang_backend.domain.payment.dto.request.PaymentRequest;
+import com.mallang.mallang_backend.domain.payment.dto.request.PaymentSimpleRequest;
+import com.mallang.mallang_backend.domain.payment.entity.PayStatus;
+import com.mallang.mallang_backend.domain.payment.service.common.PaymentService;
 import com.mallang.mallang_backend.domain.payment.service.request.PaymentRequestService;
 import com.mallang.mallang_backend.global.aop.time.TimeTrace;
 import com.mallang.mallang_backend.global.dto.RsData;
 import com.mallang.mallang_backend.global.filter.login.CustomUserDetails;
 import com.mallang.mallang_backend.global.filter.login.Login;
 import com.mallang.mallang_backend.global.swagger.PossibleErrors;
+import com.mallang.mallang_backend.global.token.JwtService;
+import com.mallang.mallang_backend.global.token.TokenPair;
+import com.mallang.mallang_backend.global.token.TokenService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+import static com.mallang.mallang_backend.domain.payment.service.common.PaymentService.*;
 import static com.mallang.mallang_backend.global.exception.ErrorCode.*;
 
 @Slf4j
@@ -28,23 +44,24 @@ import static com.mallang.mallang_backend.global.exception.ErrorCode.*;
 public class PaymentController {
 
     private final PaymentRequestService requestService;
+    private final PaymentService paymentService;
+    private final JwtService jwtService;
+    private final TokenService tokenService;
 
 
+    @Operation(
+            summary = "결제 요청 정보 생성",
+            description = "로그인한 회원의 정보와 결제 요청 데이터를 받아 결제 요청 정보를 생성합니다."
+    )
     @ApiResponse(content = @Content(schema = @Schema(implementation = PaymentRequestDocs.class)))
-    @PossibleErrors({PLAN_NOT_FOUND, ORDER_ID_CONFLICT, PAYMENT_NOT_FOUND, MEMBER_NOT_FOUND})
+    @PossibleErrors({PLAN_NOT_FOUND, PAYMENT_NOT_FOUND, MEMBER_NOT_FOUND})
     @PostMapping("/request")
-    @TimeTrace // 59 ms
+    @TimeTrace
     public ResponseEntity<RsData<PaymentRequest>> createPaymentRequest(
-            // 테스트 값 123e4567-e89b-12d3-a456-426614174000 고정
-            @Parameter(hidden = true) @RequestHeader("Idempotency-pay-key") String idempotencyKey,
             @Parameter(hidden = true) @Login CustomUserDetails userDetails,
             @Valid @RequestBody PaymentSimpleRequest simpleRequest) {
 
-        if (idempotencyKey == null || idempotencyKey.isEmpty()) {
-        }
-
         PaymentRequest request = requestService.createPaymentRequest(
-                idempotencyKey,
                 userDetails.getMemberId(),
                 simpleRequest);
 
@@ -54,17 +71,99 @@ public class PaymentController {
         return ResponseEntity.ok(response);
     }
 
-    // TODO 성공 경우
-    /**
-     * /success?paymentType={PAYMENT_TYPE}&orderId={ORDER_ID}
-     * &paymentKey={PAYMENT_KEY}&amount={AMOUNT}
-     */
+    @Operation(
+            summary = "결제 요청 성공 이후 결제 승인 로직",
+            description = "결제 승인 요청을 전송하고, 결제 상태를 업데이트하며, 권한 정보 및 JWT 토큰을 갱신합니다."
+    )
+    @TimeTrace
+    @PossibleErrors({PAYMENT_NOT_FOUND, PAYMENT_CONFIRM_FAIL, PAYMENT_CONFLICT})
+    @PostMapping("/confirm")
+    public ResponseEntity<RsData<String>> succeedPayment(
+            @Valid @RequestBody PaymentApproveRequest request,
+            HttpServletResponse response
+    ) {
 
-    // TODO 실패 경우
-    /**
-     * /fail?code={ERROR_CODE}&message={ERROR_MESSAGE}
-     * &orderId={ORDER_ID}
-     */
+        paymentService.checkIdemkeyAndSave(request.getIdempotencyKey()); // 중복 호출 확인
+        paymentService.updatePaymentStatus(
+                request.getOrderId(),
+                PayStatus.IN_PROGRESS
+        ); // 결제 승인 대기 상태로 업데이트
+
+        PaymentResponse result = paymentService.sendApproveRequest(request); // 결제 API 호출 (승인 로직 진행)
+
+        paymentService.processPaymentResult(
+                request.getOrderId(),
+                result
+        ); // DB 저장 및 성공 및 실패 이벤트 발행
+
+        MemberGrantedInfo grantedInfo = paymentService
+                .getMemberId(request.getOrderId()); // 새로운 권한 정보 발행
+
+        Long memberId = grantedInfo.memberId();
+        String roleName = grantedInfo.roleName();
+
+        setSecurityContext(memberId, roleName); // 시큐리티 객체 업데이트
+        setNewJwtTokens(response, memberId, roleName); // 토큰 정보 업데이트
+
+        RsData<String> rsp = new RsData<>(
+                "200",
+                "결제 승인 완료");
+
+        return ResponseEntity.ok(rsp);
+    }
+
+    @Operation(
+            summary = "결제 승인 실패 처리",
+            description = "결제 요청에 실패한 경우 결제 상태를 ABORTED로 변경하고 실패 로그를 기록합니다."
+    )
+    @PostMapping("/fail") // 요청 자체가 실패한 경우
+    public ResponseEntity<RsData<String>> failedPayment(
+            @Valid @RequestBody PaymentFailureRequest request
+    ) {
+        paymentService.updatePaymentStatus(
+                request.getOrderId(),
+                PayStatus.ABORTED
+        ); // 결제 상태 업데이트 및 실패 로그 기록
+
+        RsData<String> rsp = new RsData<>(
+                "200",
+                "결제 승인 실패 업데이트 완료"
+        );
+
+        return ResponseEntity.status(200).body(rsp);
+    }
+
+    // 구독한 권한에 맞는 시큐리티 인증 정보를 재발급합니다.
+    private void setSecurityContext(Long memberId,
+                                    String roleName
+    ) {
+        UsernamePasswordAuthenticationToken authentication =
+                new UsernamePasswordAuthenticationToken(
+                        memberId,
+                        null,
+                        List.of(new SimpleGrantedAuthority(roleName))
+                );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
+    // 구독한 권한에 맞는 jwt 토큰을 재발급합니다.
+    private void setNewJwtTokens(HttpServletResponse response,
+                                 Long memberId,
+                                 String roleName
+    ) {
+        TokenPair tokenPair = tokenService.createTokenPair(
+                memberId,
+                roleName
+        );
+        jwtService.setJwtSessionCookie(
+                tokenPair.getAccessToken(),
+                response
+        );
+        jwtService.setJwtPersistentCookie(
+                tokenPair.getRefreshToken(),
+                response
+        );
+    }
 }
 
 
