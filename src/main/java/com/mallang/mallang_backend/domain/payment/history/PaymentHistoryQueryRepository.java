29c042de5b1bc9e0d9a97fe34ca7ebccda76de28
sun <suunn001@gmail.com>
2025-05-15 03:59:04 +0900

    ✨ feat: 로그에 status 의 변화를 추가하기 위해 마지막 status 를 가져 오는 쿼리 작성

---
+++ src/main/java/com/mallang/mallang_backend/domain/payment/history/PaymentHistoryQueryRepository.java
@@ -0,0 +1,25 @@
+package com.mallang.mallang_backend.domain.payment.history;
+
+import com.mallang.mallang_backend.domain.payment.entity.PayStatus;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import static com.mallang.mallang_backend.domain.payment.history.QPaymentHistory.*;
+
+@Repository
+@RequiredArgsConstructor
+public class PaymentHistoryQueryRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+    // paymentId 로 PaymentHistory 에서 가장 최근의 상태를 가져옵니다.
+    public PayStatus findByPaymentLastStatus(Long paymentId){
+        return queryFactory.select(paymentHistory.status)
+                .from(paymentHistory)
+                .where(paymentHistory.payment.id.eq(paymentId))
+                .orderBy(paymentHistory.changedAt.desc())
+                .limit(1)
+                .fetchOne();
+    }
+}
