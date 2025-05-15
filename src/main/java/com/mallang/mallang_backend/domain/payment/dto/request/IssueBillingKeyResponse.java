4825d0d61040070f593f896234cb7b21bb7aac3e
sun <suunn001@gmail.com>
2025-05-16 01:52:18 +0900

    ✨ feat: DTO 객체 추가

---
+++ src/main/java/com/mallang/mallang_backend/domain/payment/dto/request/IssueBillingKeyResponse.java
@@ -0,0 +1,16 @@
+package com.mallang.mallang_backend.domain.payment.dto.request;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class IssueBillingKeyResponse {
+
+    private String billingKey;
+    private String authenticatedAt;
+}
