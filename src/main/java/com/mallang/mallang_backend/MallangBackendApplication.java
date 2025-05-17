a5dda9f85b4d5b6607c8473e2a123ce619c1940d
sun <suunn001@gmail.com>
2025-05-18 03:14:39 +0900

    🍀 Update: 업데이트 5/18

--- src/main/java/com/mallang/mallang_backend/MallangBackendApplication.java
+++ src/main/java/com/mallang/mallang_backend/MallangBackendApplication.java
@@ -3,12 +3,14 @@ package com.mallang.mallang_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 @EnableRetry
 @EnableScheduling
+@EnableJpaAuditing
 @SpringBootApplication
 public class MallangBackendApplication {
 
