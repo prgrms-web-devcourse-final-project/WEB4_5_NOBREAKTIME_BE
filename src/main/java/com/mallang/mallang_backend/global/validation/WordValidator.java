65a904eda0f3d660c9e8323cb0b848e6ab009699
Dongwoo Shin <socra167@gmail.com>
2025-05-19 15:48:36 +0900

    :bug: fix: 단어의 언어 일치 여부를 검사하는 정규식의 오류 수정

--- src/main/java/com/mallang/mallang_backend/global/validation/WordValidator.java
+++ src/main/java/com/mallang/mallang_backend/global/validation/WordValidator.java
@@ -5,7 +5,6 @@ import java.util.regex.Pattern;
 import com.mallang.mallang_backend.global.common.Language;
 
 public class WordValidator {
-	private static final Pattern WORD_PATTERN = Pattern.compile("^[a-zA-Z]+$");
 
 	public static boolean isLanguageMatch(String word, Language language) {
 		// 각 언어만 포함된 단어인지 검사
