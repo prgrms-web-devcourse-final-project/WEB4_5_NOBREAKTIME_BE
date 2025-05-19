b22f42ef15d0493932ef25823ba87a9e232bc1dd
sameom1048 <samvision99@naver.com>
2025-05-19 12:31:31 +0900

    :recycle: refactor: 불필요한 sql 파일 삭제

--- sql/6_expressionBookItem.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO PUBLIC.EXPRESSION_BOOK_ITEM (EXPRESSION_BOOK_ID,EXPRESSION_ID,CREATED_AT,IS_LEARNED) VALUES
-                                                                                                     (1,1,'2025-05-08 12:23:16.817389',false),
-                                                                                                     (1,2,'2025-05-08 12:23:28.383334',false),
-                                                                                                     (1,3,'2025-05-08 12:23:38.442742',false);
