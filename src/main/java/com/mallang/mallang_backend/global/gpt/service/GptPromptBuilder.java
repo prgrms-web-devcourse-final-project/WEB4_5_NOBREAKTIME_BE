79bf973be279a134c57e6c04f934e4db1756e6e7
sameom1048 <samvision99@naver.com>
2025-05-20 11:18:35 +0900

    :sparkles: feat: 일본어 단어 검색 프롬프트 추가

--- src/main/java/com/mallang/mallang_backend/global/gpt/service/GptPromptBuilder.java
+++ src/main/java/com/mallang/mallang_backend/global/gpt/service/GptPromptBuilder.java
@@ -3,6 +3,8 @@ package com.mallang.mallang_backend.global.gpt.service;
 public interface GptPromptBuilder {
 	String buildPromptforSearchWord(String word);
 
+	String buildPromptforSearchWordJapanese(String word);
+
 	String buildPromptForAnalyzeSentence(String sentence, String translatedSentence);
 
 	String buildPromptForAnalyzeScript(String script);
