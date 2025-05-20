a621a9e47f0eb2b031897c8be27878fdad48405f
wkdan <jmy8876@gmail.com>
2025-05-21 01:29:22 +0900

    ✅ test: 유튜브 서비스 테스트에 파라미터 추가

--- src/test/java/com/mallang/mallang_backend/global/resilience4j/CustomCircuitBreakerConfigTest.java
+++ src/test/java/com/mallang/mallang_backend/global/resilience4j/CustomCircuitBreakerConfigTest.java
@@ -13,7 +13,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
 
 import com.mallang.mallang_backend.global.exception.ServiceException;
 import com.mallang.mallang_backend.global.resilience4j.code.TestService;
@@ -38,7 +37,7 @@ class CustomCircuitBreakerConfigTest {
     @Autowired
     private CircuitBreakerRegistry registry;
 
-    @MockitoSpyBean
+    @Autowired
     private CustomCircuitBreakerConfig.CircuitBreakerEventConsumer customConsumer;
 
     @Test
