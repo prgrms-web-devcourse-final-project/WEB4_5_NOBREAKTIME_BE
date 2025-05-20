3828a6cdf6a0f6eff80523da99010f2be95580a8
gwony <moonsky1124@naver.com>
2025-05-20 11:40:26 +0900

    ✨ feat: 센트리 - 로그에 사용자 정보 기입

--- src/main/java/com/mallang/mallang_backend/global/config/WebConfig.java
+++ src/main/java/com/mallang/mallang_backend/global/config/WebConfig.java
@@ -2,8 +2,10 @@ package com.mallang.mallang_backend.global.config;
 
 import java.util.List;
 
+import com.mallang.mallang_backend.global.interceptor.SentryUserInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.mallang.mallang_backend.global.filter.login.LoginUserArgumentResolver;
@@ -15,10 +17,18 @@ import lombok.RequiredArgsConstructor;
 public class WebConfig implements WebMvcConfigurer {
 
     private final LoginUserArgumentResolver loginUserArgumentResolver;
+    private final SentryUserInterceptor sentryUserInterceptor;
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(loginUserArgumentResolver);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // 모든 경로 요청에 대해 SentryUserInterceptor 적용
+        registry.addInterceptor(sentryUserInterceptor)
+                .addPathPatterns("/**");
     }
 
 }
