b30db85b784d3c4f5211159995fc4b701d046b24
wkdan <jmy8876@gmail.com>
2025-05-25 22:04:03 +0900

    🐛 fix: ErrorCode 및 YoutubeService에서 불필요한 import 제거 및 언어 검증 로직 추가

--- src/main/java/com/mallang/mallang_backend/global/exception/ErrorCode.java
+++ src/main/java/com/mallang/mallang_backend/global/exception/ErrorCode.java
@@ -1,8 +1,9 @@
 package com.mallang.mallang_backend.global.exception;
 
+import org.springframework.http.HttpStatus;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.springframework.http.HttpStatus;
 
 @Getter
 @AllArgsConstructor
