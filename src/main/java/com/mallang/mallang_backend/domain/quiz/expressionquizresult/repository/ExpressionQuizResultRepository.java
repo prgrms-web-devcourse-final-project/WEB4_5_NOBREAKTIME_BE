d545e05da9e5e85ffe2769a1f88c6198b01803c2
sameom1048 <samvision99@naver.com>
2025-05-26 10:23:57 +0900

    :recycle: refactor: 표현함 퀴즈 결과 저장 시 동일한 퀴즈 결과 요청에 대해 update 되도록 수정

--- src/main/java/com/mallang/mallang_backend/domain/quiz/expressionquizresult/repository/ExpressionQuizResultRepository.java
+++ src/main/java/com/mallang/mallang_backend/domain/quiz/expressionquizresult/repository/ExpressionQuizResultRepository.java
@@ -1,13 +1,15 @@
 package com.mallang.mallang_backend.domain.quiz.expressionquizresult.repository;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
+import com.mallang.mallang_backend.domain.member.entity.Member;
+import com.mallang.mallang_backend.domain.quiz.expressionquiz.entity.ExpressionQuiz;
+import com.mallang.mallang_backend.domain.quiz.expressionquizresult.entity.ExpressionQuizResult;
+import com.mallang.mallang_backend.domain.sentence.expression.entity.Expression;
+import com.mallang.mallang_backend.domain.sentence.expressionbook.entity.ExpressionBook;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.mallang.mallang_backend.domain.member.entity.Member;
-import com.mallang.mallang_backend.domain.quiz.expressionquizresult.entity.ExpressionQuizResult;
-import com.mallang.mallang_backend.domain.sentence.expressionbook.entity.ExpressionBook;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 public interface ExpressionQuizResultRepository extends JpaRepository<ExpressionQuizResult, Long> {
 
@@ -22,4 +24,6 @@ public interface ExpressionQuizResultRepository extends JpaRepository<Expression
 	void deleteAllByExpressionBook(ExpressionBook expressionBook);
 
 	List<ExpressionQuizResult> findTop100ByExpressionQuiz_MemberAndCreatedAtAfterOrderByCreatedAtDesc(Member member, LocalDateTime measuredAt);
+
+	Optional<ExpressionQuizResult> findByExpressionAndExpressionBookAndExpressionQuiz(Expression expression, ExpressionBook expressionBook, ExpressionQuiz expressionQuiz);
 }
